47c6b0ac8b0d1bd268ba0b1a035c1ace40df2365
thangbk2209 <thangbk2209@gmail.com>
2020-11-11 08:59:34 +0700

    fix: encryption variables

--- organizations/ipfs_experiment.py
+++ organizations/ipfs_experiment.py
@@ -1,9 +1,9 @@
 import ipfshttpclient
 import time
 api = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001/http')
-start = time.time()
-data_mahoa = open('10MB', 'rb').read()
-cid = api.add_str(str(data_mahoa))
-done = time.time()
-elapsed = done - start
+start_time = time.time()
+plaintext_data = open('10MB', 'rb').read()
+cid = api.add_str(str(plaintext_data))
+complete_time = time.time()
+elapsed = complete_time - start_time
 print(elapsed)	
